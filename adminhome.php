9b105b37801048ea208a025512996fe50670cbb6
Arka Ganguly <arkaganguly007@gmail.com>
2022-11-12 12:27:35 +0530

    Add files via upload

--- adminhome.php
+++ adminhome.php
@@ -17,9 +17,61 @@ elseif ($_SESSION['usertype'] == 'student') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="admin.css">
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Admin Home</h1>
-    <a href="logout.php">Log Out</a>
+    <header class="header">
+        <a href="">Admin Dashboard</a>
+        <div class="logout">
+            <a href="logout.php" class="btn btn-primary">Log Out</a>
+        </div>
+    </header>
+
+    <aside>
+        <ul>
+            <li>
+                <a href="">Admission</a>
+            </li>
+
+            <li>
+                <a href="">Add Student</a>
+            </li>
+
+            <li>
+                <a href="">View Student</a>
+            </li>
+
+            <li>
+                <a href="">Add Teacher</a>
+            </li>
+
+            <li>
+                <a href="">View Teacher</a>
+            </li>
+
+            <li>
+                <a href="">Add Courses</a>
+            </li>
+
+            <li>
+                <a href="">View Courses</a>
+            </li>
+        </ul>
+    </aside>
+
+    <div class="content">
+        <h1>Sidebar Accordion:</h1>
+        <p>
+            In this example, we have added an accordion and a dropdown menu inside the side navigation.
+            
+            Click on both to understand how they differ from each other. The accordion will push down the content, while the dropdown lays over the content.
+        </p>
+    </div>
 </body>
 </html>
